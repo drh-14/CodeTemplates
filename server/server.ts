e640b3ede46514eb8e395db8a57614ef5fd6c521
drh-14 <darrenh1134@gmail.com>
2025-07-07 23:10:03 -0400

    updated CORS policy

--- server/server.ts
+++ server/server.ts
@@ -15,7 +15,7 @@ const PORT = 8000;
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://code-templates.vercel.app',
     credentials: true
 }));
 
