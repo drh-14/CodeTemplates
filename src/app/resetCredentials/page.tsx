ed315475385c2404d665abdd0f28ab5f8e63223c
drh-14 <darrenh1134@gmail.com>
2025-07-07 22:10:32 -0400

    wrapped in suspense component

--- src/app/resetCredentials/page.tsx
+++ src/app/resetCredentials/page.tsx
@@ -1,40 +1,15 @@
-'use client'
-import { Button } from '../../components/ui/button';
-import { Input } from '../../components/ui/input';
-import { useState, Suspense } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-export default function ResetCredentials(){
-    const searchParams = useSearchParams();
-    const email = searchParams.get("email");
-    console.log(email);
-    const router = useRouter();
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+import { Suspense } from 'react';
+import ResetCredentials from '../../components/ui/resetCredentials';
 
-    const changeCredentials = async () => {
-        const response = await fetch("http://localhost:8000/credentials", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({email: email, username: username, password: password})
-        });
-        if(response.status === 200){
-            router.push('/');
-        }
-    }
-
+export default function ResetCredentialsPage(){
     function Fallback(){
         return <h1></h1>
     }
 
     return(
         <Suspense fallback = {<Fallback></Fallback>}>
-        <div className = 'flex flex-col items-center mt-12 gap-8'>
-            <Input onChange = {(e) => setUsername(e.target.value)} className = 'w-1/4' placeholder = "New username"></Input>
-            <Input onChange = {(e) => setPassword(e.target.value)} className = 'w-1/4' placeholder = "New password"></Input>
-            <Button onClick = {changeCredentials}>Change Credentials</Button>
-        </div>
+            <ResetCredentials></ResetCredentials>
         </Suspense>
+        
     )
 }
---
+++ src/components/ui/resetCredentials.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { Button } from '../../components/ui/button';
+import { Input } from '../../components/ui/input';
+import { useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+export default function ResetCredentials() {
+  const searchParams = useSearchParams();
+  const email = searchParams.get('email');
+  const router = useRouter();
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const changeCredentials = async () => {
+    const response = await fetch('http://localhost:8000/credentials', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, username, password }),
+    });
+
+    if (response.status === 200) {
+      router.push('/');
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center mt-12 gap-8">
+      <Input onChange={(e) => setUsername(e.target.value)} className="w-1/4" placeholder="New username" />
+      <Input onChange={(e) => setPassword(e.target.value)} className="w-1/4" placeholder="New password" />
+      <Button onClick={changeCredentials}>Change Credentials</Button>
+    </div>
+  );
+}
